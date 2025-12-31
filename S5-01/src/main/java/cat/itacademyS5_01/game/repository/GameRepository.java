6c67690d344638eae9dbd85852e7e6e1fa78ff4b
Esther <githubEsther@gmail.com>
2025-12-31 14:05:35 +0100

    Feat: implementing the game service

--- S5-01/src/main/java/cat/itacademyS5_01/game/repository/GameRepository.java
+++ S5-01/src/main/java/cat/itacademyS5_01/game/repository/GameRepository.java
@@ -1,17 +1,14 @@
 package cat.itacademyS5_01.game.repository;
 
 import cat.itacademyS5_01.game.model.Game;
-import org.springframework.data.domain.Example;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
-public interface GameRepository  extends ReactiveMongoRepository<Game,String> {
+public interface GameRepository  extends ReactiveCrudRepository<Game,String> {
     Flux<Game> findAllByValue(String value);
     Mono<Game> findFirstByPlayer(Mono<String> player);
-    /*int playerId=0;
-    Flux<Game> gameFlux = repository.findAll(Example.of(new Game( playerId)));
-    //https://www.baeldung.com/spring-data-mongodb-reactive*/
+
 }
---
+++ S5-01/src/main/java/cat/itacademyS5_01/game/service/GameService.java
@@ -0,0 +1,33 @@
+package cat.itacademyS5_01.game.service;
+
+import cat.itacademyS5_01.game.model.Game;
+import cat.itacademyS5_01.game.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class GameService {
+    private final GameRepository gameRepository;
+
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public Mono<Game> findById(String id) {
+        return gameRepository.findById(id);
+    }
+
+    public Flux<Game> findAll() {
+        return gameRepository.findAll();
+    }
+
+    public Mono<Game> save(int playerId) {
+        Game game = new Game(playerId);
+        return gameRepository.save(game);
+    }
+
+}
