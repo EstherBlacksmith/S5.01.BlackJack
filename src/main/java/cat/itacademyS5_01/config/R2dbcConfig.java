568e5fc9d85b45d8b3f0b91f5b0aa3457756c798
Esther <githubEsther@gmail.com>
2026-01-11 18:02:57 +0100

    Feat: adding a connection initializer to the R2dbcConfig to allow the creation of the tables in the database

--- src/main/java/cat/itacademyS5_01/config/R2dbcConfig.java
+++ src/main/java/cat/itacademyS5_01/config/R2dbcConfig.java
@@ -1,9 +1,26 @@
 package cat.itacademyS5_01.config;
 
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @Configuration
 @EnableR2dbcRepositories(basePackages = "cat.itacademyS5_01.player.repository")
 public class R2dbcConfig {
+
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("schema.sql"));
+        
+        initializer.setDatabasePopulator(populator);
+        return initializer;
+    }
 }
