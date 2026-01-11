9c233420a5fab7e2699c1877765d3962959b7602
Esther <githubEsther@gmail.com>
2026-01-11 23:16:18 +0100

    Feat: new method to find the game by id

--- src/main/java/cat/itacademyS5_01/game/controller/GameController.java
+++ src/main/java/cat/itacademyS5_01/game/controller/GameController.java
@@ -1,8 +1,11 @@
 package cat.itacademyS5_01.game.controller;
 
+import cat.itacademyS5_01.exception.MissingIdentifierException;
 import cat.itacademyS5_01.exception.MissingNameException;
 import cat.itacademyS5_01.game.dto.GameRequest;
 import cat.itacademyS5_01.game.dto.GameResponse;
+import cat.itacademyS5_01.game.model.Game;
+import cat.itacademyS5_01.game.service.GameService;
 import cat.itacademyS5_01.gameplay.service.GamePlayService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +16,10 @@ import reactor.core.publisher.Mono;
 public class GameController {
 
     private final GamePlayService gamePlayService;
-
-    public GameController(GamePlayService gamePlayService) {
+    private final GameService gameService;
+    public GameController(GamePlayService gamePlayService, GameService gameService) {
         this.gamePlayService = gamePlayService;
+        this.gameService = gameService;
     }
 
     @PostMapping("/new")
@@ -26,5 +30,17 @@ public class GameController {
         }
 
         return gamePlayService.startGame(gameRequest);
+    }
+
+
+
+    @GetMapping("/new")
+    @ResponseStatus(HttpStatus.FOUND)
+    public Mono<Game> getGame(@RequestBody String gameId) throws MissingIdentifierException {
+        if (gameId.isEmpty()) {
+            throw new MissingIdentifierException("Missing game identifier");
+        }
+
+        return gameService.findById(gameId);
     }
 }
